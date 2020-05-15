403e7c9d88546ee970be411270607d2a186478d6
Chris Nelson <nelsonc5@flip3.engr.oregonstate.edu>
2020-05-15 10:09:45 -0700

    join table attempt

--- website_handlebars/mechanicShop.js
+++ website_handlebars/mechanicShop.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+
 /**************************************************************
  * Dashboard
  * ************************************************************/
@@ -21,6 +22,43 @@ app.get('/home', function(req, res, next) {
     context.title = 'Dashboard';
     res.render('home', context);
 });
+
+
+/**************************************************************
+ * Dashboard - CHRIS ATEMPT
+ * ************************************************************/
+/*
+app.get('/home', function(req, res, next) {
+    var context = {};
+    //var tableName = 'customers'; 
+    context.addHref = '/home';
+    //context.title = 'Customers';
+    var sql = 'SELECT customers.f_name AS first_name, customers.l_name AS last_name,'+
+    'work_tasks.name AS work_task, work_orders.start_date'+
+    'AS start_date, mechanics.f_name AS mechanic_f_name, mechanics.l_name AS mechanic_l_name'+
+    'FROM repair_orders JOIN cars ON repair_orders.car_id = cars.id'+
+    'JOIN customers ON cars.customer_id = customers.id'+
+    'JOIN work_orders ON repair_orders.id = work_orders.repair_order_id'+
+    'JOIN work_tasks ON work_orders.work_task_id = work_tasks.id'+
+    'JOIN mechanics ON work_orders.mechanic_id = mechanics.id;';
+    //var inserts = [tableName, 'Column_name', 'Information_schema.columns', 'Table_name', tableName];
+    mysql.pool.query(sql, function(err, results){
+    console.log(sql);
+        
+        if(err){
+            throw err;
+        }else {
+            renderPage(results);
+        }
+    });
+    function renderPage(results) {
+        context.dataRows = results[0];
+        context.dataColumns = results[1];
+        res.render('home', context);
+    }
+});
+*/
+
 
 /**************************************************************
  * Customers
