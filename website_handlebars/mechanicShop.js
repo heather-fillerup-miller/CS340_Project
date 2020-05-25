cf138427e10414aac5feb46973be33a67b62dc34
Heather Fillerup <filleruh@flip3.engr.oregonstate.edu>
2020-05-25 13:02:17 -0700

    Updated Dashboard Query

--- website_handlebars/mechanicShop.js
+++ website_handlebars/mechanicShop.js
@@ -56,11 +56,12 @@ app.get('/home', function(req, res, next) {
             + "CONCAT(cars.model_year, ' ', cars.make, ' ', cars.model_name ) AS car_description, "
             + "work_tasks.name AS current_task, work_orders.start_date AS start_date, "
             + "CONCAT(mechanics.f_name, ' ', mechanics.l_name) AS mechanic_name "
-            + "FROM repair_orders JOIN cars ON repair_orders.car_id = cars.id "
-            + "JOIN customers ON cars.customer_id = customers.id "
-            + "JOIN work_orders ON repair_orders.id = work_orders.repair_order_id AND work_orders.end_date IS NULL "
-            + "JOIN work_tasks ON work_orders.work_task_id = work_tasks.id "
-            + "JOIN mechanics ON work_orders.mechanic_id = mechanics.id "
+            + "FROM repair_orders "
+            + "LEFT JOIN cars ON repair_orders.car_id = cars.id "
+            + "LEFT JOIN customers ON cars.customer_id = customers.id "
+            + "LEFT JOIN work_orders ON repair_orders.id = work_orders.repair_order_id AND work_orders.end_date IS NULL "
+            + "LEFT JOIN work_tasks ON work_orders.work_task_id = work_tasks.id "
+            + "LEFT JOIN mechanics ON work_orders.mechanic_id = mechanics.id "
             + "GROUP BY work_orders.start_date DESC, customers.f_name, work_tasks.id;";
     mysql.pool.query(sql, function(err, results){
         if(err){
